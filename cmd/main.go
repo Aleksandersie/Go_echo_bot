b6c5395cd6a0686693f44ab5297badf0a003d4f8
Aleksandersie <aleksander.sie@gmail.com>
2023-11-19 14:20:50 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	token, _ := os.LookupEnv("TG_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			name := update.Message.Chat.FirstName
+
+			msgAddOn := " ты написал "
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, name+msgAddOn+update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		}
+	}
+}
